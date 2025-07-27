17f526421da6028fea397200316344bd7266bb4d
aloshvp <aloshvp12@gmail.com>
2025-07-28 00:05:46 +0530

    data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Carousel from './components/Carousel';
+import { PHOTO_API_URL } from './utils/constant';
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(PHOTO_API_URL);
+      const data = await response.json();
+      console.log(data)
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <>
-      Carousel
+      <Carousel
+        // images={[]}
+        isLoading={isLoading}
+      // imgPerSlide={ }
+      // imageLimit={ }
+      // customPrevButton={ }
+      // customNextButton={ }
+      />
     </>
   )
 }
